5479b29f90468af9336f4fe48a9bb545b05b2a8b
Allan Guerchoux <allan.gcx@gmail.com>
2024-08-21 18:28:37 +0200

    ajout du component footer

---
+++ my-app/src/components/Copyright.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Copyright = () => {
+    return (
+        <div>
+            <p>© 2020 Kava.All right reserved</p>
+        </div>
+    );
+};
+
+export default Copyright;
---
+++ my-app/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Logo from './Logo'
+import Copyright from './copyright';
+const Footer = () => {
+    return (
+        <div>
+            <Logo />
+            <Copyright />
+        </div>
+    );
+};
+
+export default Footer;
